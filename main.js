0855b747aa7653cdcf82147f09fef728eaef09d8
Ashour100 <interinstitute5@gmail.com>
2022-02-26 16:08:18 +0100

    New feature added

--- main.js
+++ main.js
@@ -7,8 +7,13 @@ let VerRis=document.getElementById("Ver_ris");
 let num_verifica;
 let vincitore=document.getElementById("il_vin");
 
+
 button.addEventListener("click", function(){
     verifier = 0;
+    if(Email.value==""){
+        alert("non hai inserito niente");
+        location.reload();
+    }
     for(let i = 0; i < EmailList.length; i++ ){
         if(EmailList[i]==Email.value){
             verifier = 1;
@@ -43,3 +48,42 @@ button.addEventListener("click", function(){
 })
 
 
+////////////////////////Adding new user
+let newUserLink= document.querySelector('#newUserLink');
+let existingUserDiv= document.querySelector("#exsisting_user");
+let newUserDiv= document.getElementById("new_user");
+let newMail= document.getElementById("new_mail");
+let addMail=document.querySelector("#Add");
+
+newUserLink.addEventListener("click", function(){
+    existingUserDiv.classList.add("d-none");
+    newUserDiv.classList.remove("d-none");
+})
+addMail.addEventListener("click", function(){
+    if(newMail.value==""){
+        alert("non hai inserito niente");
+        location.reload();
+    }
+    verifier = 0;
+    for(let i = 0; i < EmailList.length; i++ ){
+        if(EmailList[i]==newMail.value){
+            verifier = 1;
+        }
+    }
+    if(verifier==1)
+        alert('You are already in our database');
+    else if (verifier==0&&!(newMail.value=="")){
+        alert("Email registered successfully");
+        EmailList.push(newMail.value);
+    }
+    
+    existingUserDiv.classList.remove("d-none");
+    newUserDiv.classList.add("d-none");
+    console.log(EmailList);
+    ///////////reset
+    VerRis.innerHTML="";
+    document.getElementById("user_num").innerHTML="";
+    document.getElementById("comp_num").innerHTML="";
+    vincitore.innerHTML="";
+    newMail.value="";
+})
